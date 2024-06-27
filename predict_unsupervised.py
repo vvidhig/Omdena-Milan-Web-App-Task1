8c493e9cc2658331b1d4b5128cbeebfce4a95c95
Vidhi <vidhidoesnotreply@gmail.com>
2024-06-28 00:26:59 +0530

    added predict_unsupervised.py

---
+++ predict_unsupervised.py
@@ -0,0 +1,42 @@
+import pickle
+import numpy as np
+import pandas as pd
+from data_fetcher import DataFetcher
+
+# Load the trained model
+with open('XGBClassifier_Pipeline_Optuna_Vidhi.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Initialize DataFetcher
+# data_fetcher = DataFetcher('dataset/Merged_2014.csv')
+data_fetcher = DataFetcher('dataset/MergedData_2023.csv')
+
+
+def predict(latitude, longitude):
+    # Fetch additional data based on latitude and longitude
+    additional_data = data_fetcher.fetch_data(latitude, longitude)
+
+    if additional_data is None:
+        return "No data found for the given latitude and longitude."
+
+    # Prepare the input data for the model as a Pandas DataFrame
+    input_data = pd.DataFrame({
+        'Latitude': [latitude],
+        'Longitude': [longitude],
+        'Zone': [additional_data['Zone']],
+        'NDVI': [additional_data['NDVI']],
+        'landuse': [additional_data['landuse']],
+        'LST': [additional_data['LST']],
+        'NDBI': [additional_data['NDBI']],
+        'NDWI': [additional_data['NDWI']],
+        'Roughness': [additional_data['Roughness']],
+        'SAVI': [additional_data['SAVI']],
+        'Slope': [additional_data['Slope']],
+        'SMI': [additional_data['SMI']],
+        'solar_radiation': [additional_data['solar_radiation']]
+    })
+
+    # Make the prediction
+    prediction = model.predict(input_data)
+
+    return prediction[0]
