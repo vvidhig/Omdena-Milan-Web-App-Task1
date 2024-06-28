aeaa8c979b5a3dc721dd08646b116b22bb711edb
Vidhi <vidhidoesnotreply@gmail.com>
2024-06-28 23:56:26 +0530

    final commit

--- predict.py
+++ predict.py
@@ -1,17 +1,15 @@
 import pickle
 import numpy as np
 import pandas as pd
-from data_fetcher import DataFetcher
 import streamlit as st
+from data_fetcher import DataFetcher
 
 # Load the trained model
 with open('XGBClassifier_Pipeline_Optuna_Vidhi.pkl', 'rb') as f:
     model = pickle.load(f)
 
 # Initialize DataFetcher
-# data_fetcher = DataFetcher('dataset/Merged_2014.csv')
 data_fetcher = DataFetcher('dataset/MergedData_2023.csv')
-
 
 def predict(latitude, longitude):
     # Fetch additional data based on latitude and longitude
@@ -19,46 +17,47 @@ def predict(latitude, longitude):
 
     if additional_data is None:
         st.error("No data found for the given latitude and longitude.")
-
-    # Prepare the input data for the model as a Pandas DataFrame
-    input_data = pd.DataFrame({
-        'Latitude': [latitude],
-        'Longitude': [longitude],
-        'Zone': [additional_data['Zone']],
-        'NDVI': [additional_data['NDVI']],
-        'landuse': [additional_data['landuse']],
-        'LST': [additional_data['LST']],
-        'NDBI': [additional_data['NDBI']],
-        'NDWI': [additional_data['NDWI']],
-        'Roughness': [additional_data['Roughness']],
-        'SAVI': [additional_data['SAVI']],
-        'Slope': [additional_data['Slope']],
-        'SMI': [additional_data['SMI']],
-        'solar_radiation': [additional_data['solar_radiation']]
-    })
-
-    st.write("### Additional Features:")
-    features = [
-        ('Zone', additional_data['Zone']),
-        ('NDVI', additional_data['NDVI']),
-        ('Land Use', additional_data['landuse']),
-        ('LST', additional_data['LST']),
-        ('NDBI', additional_data['NDBI']),
-        ('NDWI', additional_data['NDWI']),
-        ('Roughness', additional_data['Roughness']),
-        ('SAVI', additional_data['SAVI']),
-        ('Slope', additional_data['Slope']),
-        ('SMI', additional_data['SMI']),
-        ('Solar Radiation', additional_data['solar_radiation'])
-    ]
-
-    cols = st.columns(3)
-    for i, (name, value) in enumerate(features):
-        cols[i % 2].metric(label=name, value=value)
-    # Make the prediction
-    prediction = model.predict(input_data)
-
-    if prediction[0] == 0:
-        st.markdown("<p style='color:red;'>The area is Not Suitable for Urban Farming</p>", unsafe_allow_html=True)
     else:
-        st.markdown("<p style='color:green;'>The area is Suitable for Urban Farming</p>", unsafe_allow_html=True)
+        # Prepare the input data for the model as a Pandas DataFrame
+        input_data = pd.DataFrame({
+            'Latitude': [latitude],
+            'Longitude': [longitude],
+            'Zone': [additional_data['Zone']],
+            'NDVI': [additional_data['NDVI']],
+            'landuse': [additional_data['landuse']],
+            'LST': [additional_data['LST']],
+            'NDBI': [additional_data['NDBI']],
+            'NDWI': [additional_data['NDWI']],
+            'Roughness': [additional_data['Roughness']],
+            'SAVI': [additional_data['SAVI']],
+            'Slope': [additional_data['Slope']],
+            'SMI': [additional_data['SMI']],
+            'solar_radiation': [additional_data['solar_radiation']]
+        })
+
+        st.write("### Additional Features:")
+        features = [
+            ('Zone', additional_data['Zone']),
+            ('NDVI', additional_data['NDVI']),
+            ('Land Use', additional_data['landuse']),
+            ('LST', additional_data['LST']),
+            ('NDBI', additional_data['NDBI']),
+            ('NDWI', additional_data['NDWI']),
+            ('Roughness', additional_data['Roughness']),
+            ('SAVI', additional_data['SAVI']),
+            ('Slope', additional_data['Slope']),
+            ('SMI', additional_data['SMI']),
+            ('Solar Radiation', additional_data['solar_radiation'])
+        ]
+
+        cols = st.columns(3)
+        for i, (name, value) in enumerate(features):
+            cols[i % 3].metric(label=name, value=value)
+
+        # Make the prediction
+        prediction = model.predict(input_data)
+
+        if prediction[0] == 0:
+            st.error("The area is Not Suitable for Urban Farming", unsafe_allow_html=True)
+        else:
+            st.success("The area is Suitable for Urban Farming", unsafe_allow_html=True)
