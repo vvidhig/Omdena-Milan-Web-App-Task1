7af082bb4e2719a7a64f1be3bc92316d67c656fd
Vidhi <vidhidoesnotreply@gmail.com>
2024-07-02 01:42:33 +0530

    final commit

--- A_home.py
+++ A_home.py
@@ -56,6 +56,7 @@ def app():
 
     # Ensure the correct path to the image
     image_path = os.path.join(os.path.dirname(__file__), 'Images', 'home_page_gif.gif')
+    
 
     # Add your GIF here with custom caption color
     st.markdown(
@@ -73,6 +74,22 @@ def app():
         This application helps you analyze and visualize the suitability of different areas for agriculture using latitude and longitude inputs. Our powerful machine learning model predicts the agricultural suitability based on various factors.
     </div>
     """, unsafe_allow_html=True)
+    
+    st.markdown("""
+    <div class="custom-text">
+    <h3>Introduction</h3>
+        <p>Urban farming plays a crucial role in the fight against climate change by supporting sustainable food production within cities. As urbanization continues to expand, integrating agriculture into urban areas not only strengthens local food security but also reduces transportation emissions and the carbon footprint associated with conventional farming practices.</p>
+        <p>Additionally, urban farming contributes to the creation of green spaces, helps mitigate the urban heat island effect, and improves air quality. By connecting communities with locally grown food, it encourages environmental responsibility and raises awareness about the impact of traditional agriculture on climate change. Embracing urban farming is essential for the development of environmentally friendly and resilient cities, while also fostering a sustainable future in the face of global climate challenges.</p>
+    </div>
+    """, unsafe_allow_html=True)
+    
+    st.markdown("""
+    <div class="custom-text">
+    <h3>Project Overview</h3>
+        <p>Urban farming plays a crucial role in the fight against climate change by supporting sustainable food production within cities. As urbanization continues to expand, integrating agriculture into urban areas not only strengthens local food security but also reduces transportation emissions and the carbon footprint associated with conventional farming practices.</p>
+    </div>
+    """, unsafe_allow_html=True)
+    
 
     st.markdown("""
     <div class="custom-text">
